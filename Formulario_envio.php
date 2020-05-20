7c502248cfb60dd260b6e1135354266baf15ead6
Eisie <56452376+Eisie@users.noreply.github.com>
2020-05-19 20:15:07 -0400

    Inicial

---
+++ Formulario_envio.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+<title>Documento sin título</title>
+
+<style>
+
+
+table{
+	margin:auto;
+	left:0;
+}
+
+
+</style>
+</head>
+
+<body>
+<div class="row">
+ <div class="col-lg-8 mb-5" >
+<form name="formulario_contacto" method="post" action="Enviar_mail.php">
+	<div class="form-group row">
+        <div class="col-md-6 mb-4 mb-lg-0">
+ <input type="text" name="nombre" class="form-control" placeholder="Nombre">
+
+</div>
+    <div class="col-md-6">
+
+<input type="text" name="apellido" class="form-control" placeholder="Apellido">
+</div>
+  </div>
+  
+  <div class="form-group row">
+    <div class="col-md-12">
+<input type="text" name="email" class="form-control" placeholder="Introduce tu correo electrónico">
+ </div>
+     </div>
+<div class="form-group row">
+    <div class="col-md-12">
+
+<input type="text" name="tfno" class="form-control" placeholder="Número de celular">
+</div>
+     </div>
+
+
+<div class="form-group row">
+    <div class="col-md-12">
+    <input type="text" name="asunto" class="form-control" placeholder="asunto:">
+</div>
+     </div>
+<div class="form-group row">
+        <div class="col-md-12">
+
+<textarea name="comentarios" class="form-control" placeholder="Escribe tu mensaje." cols="30" rows="10"></textarea>
+<td colspan="2" style="text-align:center">
+	</div>
+    </div>
+
+ <input type="submit" class="btn btn-block btn-primary text-white py-3 px-5" value="Enviar">
+    </div>
+              </div>
+            </form>
+          </div>           
+
+
+
+
+
+</body>
+</html>
