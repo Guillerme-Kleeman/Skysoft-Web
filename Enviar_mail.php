eb9972cb630a928b31def5e131de7dba37c94bc0
Eisie <56452376+Eisie@users.noreply.github.com>
2021-11-02 12:17:48 -0300

    Update Enviar_mail.php

--- Enviar_mail.php
+++ Enviar_mail.php
@@ -3,23 +3,7 @@ $texto_mail ="Nombre: ". $_POST["nombre"] ." ". $_POST["apellido"]. ". ";
 $texto_mail .= "Celular: ". $_POST["tfno"]." ";
 $texto_mail .="Correo: ". $_POST["email"]." ";
 $texto_mail .="Mensaje: ". $_POST["comentarios"]." ";
-$destinatario="erikasilvero13@gmail.com";
-
-$asunto=$_POST["asunto"];
-$headers="MIME-Version: 1.0\r\n";
-$headers.="Content-type: text/html; charset=iso-8859-1\r\n";
-$headers.="From: Prueba Erika <erikasilvero13@gmail.com>\r\n";
-$exito=mail($destinatario, $asunto, $texto_mail, $headers);
-
-if ($exito) {
-	
-	header("Location:alert.html");	
-}
-else{
-	
-	header("Location:alerta_fallo.html");	
-}
-
+$destinatario="erikasilvero13@gmail.com"
 
 
 
